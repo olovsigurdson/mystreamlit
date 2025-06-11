25074efcca7e21ef5771ee0801714e7ff5d60c5a
sigurolo <olov.sigurdson@tietoevry.com>
2025-06-11 14:47:17 +0200

    Nytt!

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import pandas
+
+data = {
+    'Series_1': [1, 2, 3, 4, 5, 7],
+    'Series_2': [10, 30, 40, 100, 250]
+}
 
 st.title('Olovs first app')
 st.subheader('Testar Streamlit!!')
 st.write('''Första webappen
 Testar!! mer
 ''')
+
+df = pandas.DataFrame(data)
+st.write(df)
